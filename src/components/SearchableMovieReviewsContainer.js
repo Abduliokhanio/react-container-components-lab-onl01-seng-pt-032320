bd79069298a7281d4a928de93025c19059f398ca
Abdul Khan <akhan17@outlook.com>
2020-12-02 21:06:17 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,12 +1,3 @@
-import React, { Component } from 'react';
-import 'isomorphic-fetch';
-import MovieReviews from './MovieReviews'
-
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
-
-// Code SearchableMovieReviewsContainer Here
 import React from 'react';
 import MovieReviews from './MovieReviews';
 import SearchInput from './SearchInput';
